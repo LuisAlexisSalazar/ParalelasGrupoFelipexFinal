cc5baa8119066c3354a51fb4df2d933294e49bff
LuisAlexisSalazar <kirito_SWO@outlook.com>
2021-11-16 13:55:31 -0500

    Encriptado con Mapa

---
+++ test.py
@@ -0,0 +1,10 @@
+from cv2 import cv2	
+
+
+# imageName = "C:/Users/Admin/Encriptación de Imagenes con Mapa Caotico/CUDA-Image-Encryption-CUDA-Saswat/src-parallel/input/lena.png"
+# imageName = "lena.png"
+
+# cv2.imshow(imageName,image)
+image = cv2.imread(imageName)
+cv2.imshow(imageName,image)
+cv2.waitKey(0)
